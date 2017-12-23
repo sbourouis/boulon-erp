2c32a27d4d2ace9ba8c739e3507b9b4c13ee0078
sbourouis <bourouis.sabrina@laposte.net>
2017-12-23 19:08:54 +0100

    test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import {Supplier} from "../../models/supplier";
 import {CommandsService} from "../../services/commands.service";
 import {SuppliersService} from "../../services/suppliers.service";
 import {StocksService} from "../../services/stocks.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -14,25 +15,29 @@ import {StocksService} from "../../services/stocks.service";
   styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
+  cmds$: Observable<Command[]>;
+  cmd: Command[];
 
   constructor(private util: UtilsService, private cs: CommandsService, private ss: SuppliersService, private sts: StocksService) {
 
-    let cmds: Command[] = [];
-    let stocks: Stock[] = [ ];
-
-    let suppliers$: Supplier[] = [];
-    cs.index().map((commandes: Command[]) => cmds = commandes);
-    ss.index().subscribe( res => suppliers$ = res);
-    sts.index().subscribe(res => stocks = res);
-
-    console.log(cmds);
-    // cmds = cs.index();
-    let test = new Date();
-    console.log(test);
-    test.setHours(test.getHours() + 10);
-    console.log(test);
+    cs.index().subscribe(cmds => this.cmd = cmds);
+    this.cmds$ = cs.index();
+    // let cmds: Command[] = [];
+    // let stocks: Stock[] = [ ];
+    //
+    // let suppliers$: Supplier[] = [];
+    // cs.index().map((commandes: Command[]) => cmds = commandes);
+    // ss.index().subscribe( res => suppliers$ = res);
+    // sts.index().subscribe(res => stocks = res);
+    //
+    // console.log(cmds);
+    // // cmds = cs.index();
+    // let test = new Date();
+    // console.log(test);
+    // test.setHours(test.getHours() + 10);
+    // console.log(test);
     // console.log(util.getMonthBetween(new Date('1/12/2014'), new Date('9/12/2015') ));
-    console.log(util.getOrdersDates(cmds, stocks, suppliers$));
+    // console.log(util.getOrdersDates(cmds, stocks, suppliers$));
     // console.log(util.getTasksFromProduct(product, 1000, new Date));
     // util.getTasksFromCommands(cmds)); // util.IsOrderPossible(cmds, new Date('December 7, 2017 11:13:00'), stocks));
     // console.log(util.businessDaysBetweenDates(new Date('December 11, 2017 11:13:00'), new Date()));
